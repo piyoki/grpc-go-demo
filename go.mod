2db13ad06b2fc79cb09e470f192d9e9819fef918
yqlbu <yqlbu@Bu.edu>
2021-04-19 17:43:04 +0800

    updated files

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/yqlbu/grpc-go-demo
 
 go 1.16
+
+require (
+	golang.org/x/net v0.0.0-20210415231046-e915ea6b2b7d
+	google.golang.org/grpc v1.37.0
+	google.golang.org/protobuf v1.26.0
+)
--- server.go
+++ server.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 
+	"github.com/yqlbu/grpc-go-demo"
 	"google.golang.org/grpc"
 )
 
 func main() {
 
-	lis, err := net.Listen("tcp", ":9000")
+	fmt.Println("Go gRPC Beginners Tutorial!")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	s := chat.Server{}
+
 	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
